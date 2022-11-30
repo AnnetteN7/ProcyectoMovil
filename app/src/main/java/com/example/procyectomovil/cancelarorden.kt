ea74f33b207ce9daac8e41dc432d9a959a53ef8b
Emmanuel Malavassi <malavassiemmanuel@gmail.com>
2022-11-30 00:45:58 -0600

    Commit Cancelar Orden, arreglo login y register, Mi Orden

--- app/src/main/java/com/example/procyectomovil/cancelarorden.kt
+++ app/src/main/java/com/example/procyectomovil/cancelarorden.kt
@@ -1,11 +1,4 @@
 package com.example.procyectomovil
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-
-class cancelarorden : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cancelarorden)
-    }
+class cancelarOrden {
 }
