4c27032ced045528a7f97fe8f97f43818acc8e1c
Emmanuel Malavassi <malavassiemmanuel@gmail.com>
2022-11-30 01:40:57 -0600

    Commit detalle

---
+++ app/src/main/java/com/example/procyectomovil/ShowDetailActivty.kt
@@ -0,0 +1,11 @@
+package com.example.procyectomovil
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowDetailActivty : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_detail_activty)
+    }
+}
